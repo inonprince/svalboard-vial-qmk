3b244462fdd9d2f171f6f2890416130156aa0783
Inon Prince <inon.prince@oracle.com>
2026-05-26 00:08:06 -0700

    Make glove80 FUNC F keys mnemonic

--- keyboards/svalboard/keymaps/glove80_dvorak/keymap.c
+++ keyboards/svalboard/keymaps/glove80_dvorak/keymap.c
@@ -641,10 +641,10 @@ const uint16_t PROGMEM keymaps[DYNAMIC_KEYMAP_LAYER_COUNT][MATRIX_ROWS][MATRIX_C
         /*R2*/ KC_F11          , KC_VOLU          , KC_MUTE         , KC_F12          , KC_VOLD          , KC_NO ,
         /*R3*/ KC_PSCR         , KC_BRIU          , KC_NO           , KC_BRID         , KC_PAUSE         , KC_NO ,
         /*R4*/ KC_APP          , KC_EJCT          , KC_NO           , KC_WHOM         , KC_CALC          , KC_NO ,
-        /*L1*/ KC_F7           , KC_F8            , KC_F9           , KC_F10          , KC_F24           , KC_NO ,
-        /*L2*/ KC_F5           , KC_F6            , KC_F11          , KC_F12          , KC_F23           , KC_NO ,
-        /*L3*/ KC_F3           , KC_F14           , KC_F13          , KC_F4           , KC_F22           , KC_NO ,
-        /*L4*/ KC_F1           , KC_F16           , KC_F15          , KC_F2           , KC_F21           , KC_NO ,
+        /*L1*/ KC_F1           , KC_F9            , KC_F13          , KC_F5           , KC_F21           , KC_NO ,
+        /*L2*/ KC_F2           , KC_F10           , KC_F14          , KC_F6           , KC_F22           , KC_NO ,
+        /*L3*/ KC_F3           , KC_F11           , KC_F15          , KC_F7           , KC_F23           , KC_NO ,
+        /*L4*/ KC_F4           , KC_F12           , KC_F20          , KC_F8           , KC_F24           , KC_NO ,
 
         /*     Down            Pad      Up       Nail     Knuckle  DoubleDown */
         /*RT*/ KC_TRNS       , KC_TRNS, KC_TRNS, SV_LOCK_FUNC, KC_TRNS, KC_TRNS ,
